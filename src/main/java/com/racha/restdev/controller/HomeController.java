92cd8c18e9d300205715680b9a085c68c41ac626
Racha011 <ratshalingwaadivhaho106@gmail.com>
2024-11-17 17:23:47 +0200

    deployment update

--- src/main/java/com/racha/restdev/controller/HomeController.java
+++ src/main/java/com/racha/restdev/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController 
-@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+@CrossOrigin(origins = "https://photovault011.onrender.com", maxAge = 3600)
 public class HomeController {
     @GetMapping("/api/v1")
     public String home() {
